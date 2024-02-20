6c42923b1da4020a34cc9e4dfa79db0e7a549e32
Wendryo <wendryo.sales@gmail.com>
2024-02-19 22:38:34 -0300

    feat: add Global Pipes to validations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // https://docs.nestjs.com/pipes#global-scoped-pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
