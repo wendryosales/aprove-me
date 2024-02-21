64507775b7093a48717a750b9a14750d949098ab
Wendryo <wendryo.sales@gmail.com>
2024-02-21 15:32:06 -0300

    feat: export assignor props

--- backend/src/domain/enterprise/entities/assignor.ts
+++ backend/src/domain/enterprise/entities/assignor.ts
@@ -1,7 +1,7 @@
 import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-id";
 
-interface AssignorProps {
+export interface AssignorProps {
   document: string;
   email: string;
   phone: string;
