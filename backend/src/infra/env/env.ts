748fb89618dec55ea8fcb38767907bf76c6c1c58
Wendryo <wendryo.sales@gmail.com>
2024-02-21 13:30:18 -0300

    refactor: get DatabaseUrl from .env

--- backend/src/infra/env/env.ts
+++ backend/src/infra/env/env.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
+  DATABASE_URL: z.string().url().default("file:./dev.db"), // In case forgot to set DATABASE_URL
   PORT: z.coerce.number().optional().default(3001),
 });
 
