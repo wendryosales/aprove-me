dafaebfb0ad82042f65f0793655df474dcadd34a
Wendryo <wendryo.sales@gmail.com>
2024-02-21 15:51:12 -0300

    test: new test e2e to get payable by id

--- backend/src/infra/http/controllers/payable.controller.e2e-spec.ts
+++ backend/src/infra/http/controllers/payable.controller.e2e-spec.ts
@@ -3,21 +3,27 @@ import request from "supertest";
 import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { AppModule } from "@/infra/app.module";
-import { makePayable } from "@test/factories/make-payable";
+import { makePayable, PayableFactory } from "@test/factories/make-payable";
 import { PrismaService } from "@/infra/database/prisma/prisma.service";
-import { makeAssignor } from "@test/factories/make-assignor";
+import { AssignorFactory, makeAssignor } from "@test/factories/make-assignor";
+import { DatabaseModule } from "@/infra/database/database.module";
 
 describe("PayableController (e2e)", () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let assignorFactory: AssignorFactory;
+  let payableFactory: PayableFactory;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [AssignorFactory, PayableFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
     prisma = moduleRef.get(PrismaService);
+    assignorFactory = moduleRef.get(AssignorFactory);
+    payableFactory = moduleRef.get(PayableFactory);
     await app.init();
   });
 
@@ -65,7 +71,22 @@ describe("PayableController (e2e)", () => {
 
   describe("GET /integrations/payable/:id", () => {
     it("should get a payable by id", async () => {
-      // ...
+      const assignor = await assignorFactory.makePrismaAssignor();
+      const payable = await payableFactory.makePrismaPayable({
+        assignorId: assignor.id,
+      });
+
+      const response = await request(app.getHttpServer()).get(
+        `/integrations/payable/${payable.id}`,
+      );
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({
+        id: payable.id.toString(),
+        value: payable.value,
+        emissionDate: payable.emissionDate.toISOString(),
+        assignorId: payable.assignorId.toString(),
+      });
     });
   });
 });
