dcae1463b1bfe3c124ea9f3ce20220efface5dfb
Wendryo <wendryo.sales@gmail.com>
2024-02-21 23:09:41 -0300

    feat: create presenter to payableController

--- backend/src/infra/http/controllers/payable.controller.ts
+++ backend/src/infra/http/controllers/payable.controller.ts
@@ -1,6 +1,8 @@
 import {
+  BadRequestException,
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   Param,
@@ -10,18 +12,29 @@ import {
 import { CreatePayableDto } from "../dtos/create-payable.dto";
 import { CreatePayableUseCase } from "@/domain/application/use-cases/create-payable";
 import { GetPayableByIdUseCase } from "@/domain/application/use-cases/get-payable-by-id";
+import { DeletePayableUseCase } from "@/domain/application/use-cases/delete-payable";
+import { PayableAssignorPresenter } from "@/infra/http/presenters/payable-assignor-presenter";
 
 @Controller("/integrations/payable")
 export class PayableController {
   constructor(
     private createPayableUseCase: CreatePayableUseCase,
     private getPayableByIdUseCase: GetPayableByIdUseCase,
+    private deletePayableUseCase: DeletePayableUseCase,
   ) {}
 
   @Post()
   @HttpCode(201)
-  createPayable(@Body() body: CreatePayableDto) {
-    return this.createPayableUseCase.execute(body);
+  async createPayable(@Body() body: CreatePayableDto) {
+    const result = await this.createPayableUseCase.execute(body);
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const { payable, assignor } = result.value;
+
+    return PayableAssignorPresenter.toHTTP(payable, assignor);
   }
 
   @Get("/:id")
@@ -31,5 +44,20 @@ export class PayableController {
     id: string,
   ) {
     return this.getPayableByIdUseCase.execute(id);
+  }
+
+  @Delete(":id")
+  @HttpCode(204)
+  async deletePayable(
+    @Param("id", new ParseUUIDPipe({ version: "4", errorHttpStatusCode: 400 }))
+    payableId: string,
+  ) {
+    const result = await this.deletePayableUseCase.execute({
+      payableId: payableId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
   }
 }
