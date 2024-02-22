c23df6b7c9d4d9e5fe589faa56172522634adce6
Wendryo <wendryo.sales@gmail.com>
2024-02-22 10:18:59 -0300

    feat: register filters providers

--- backend/src/infra/http/http.module.ts
+++ backend/src/infra/http/http.module.ts
@@ -2,12 +2,11 @@ import { Module } from "@nestjs/common";
 import { PayableController } from "./controllers/payable.controller";
 import { CreatePayableUseCase } from "@/domain/application/use-cases/create-payable";
 import { DatabaseModule } from "@/infra/database/database.module";
-import { APP_FILTER } from "@nestjs/core";
-import { PrismaExceptionFilter } from "@/infra/http/filters/prisma-exception.filter";
 import { GetPayableByIdUseCase } from "@/domain/application/use-cases/get-payable-by-id";
 import { AssignorController } from "@/infra/http/controllers/assignor.controller";
 import { GetAssignorByIdUseCase } from "@/domain/application/use-cases/get-assignor-by-id";
 import { DeletePayableUseCase } from "@/domain/application/use-cases/delete-payable";
+import { filtersProviders } from "@/infra/http/filters/filters-providers";
 
 @Module({
   imports: [DatabaseModule],
@@ -17,10 +16,7 @@ import { DeletePayableUseCase } from "@/domain/application/use-cases/delete-paya
     GetPayableByIdUseCase,
     GetAssignorByIdUseCase,
     DeletePayableUseCase,
-    {
-      provide: APP_FILTER,
-      useClass: PrismaExceptionFilter,
-    },
+    ...filtersProviders,
   ],
 })
 export class HttpModule {}
