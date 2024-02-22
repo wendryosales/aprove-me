ae8428b33c546d0f89d72ceaeac28d2c548e835c
Wendryo <wendryo.sales@gmail.com>
2024-02-22 15:58:05 -0300

    test: e2e to update and delete assignor

--- backend/src/infra/http/controllers/assignor.controller.e2e-spec.ts
+++ backend/src/infra/http/controllers/assignor.controller.e2e-spec.ts
@@ -6,11 +6,12 @@ import { AppModule } from "@/infra/app.module";
 import { PayableFactory } from "@test/factories/make-payable";
 import { AssignorFactory } from "@test/factories/make-assignor";
 import { DatabaseModule } from "@/infra/database/database.module";
-import { beforeEach } from "vitest";
+import { beforeEach, describe } from "vitest";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 
 describe("AssignorController (e2e)", () => {
   let app: INestApplication;
-
+  let prisma: PrismaService;
   let assignorFactory: AssignorFactory;
 
   beforeEach(async () => {
@@ -20,6 +21,7 @@ describe("AssignorController (e2e)", () => {
     }).compile();
 
     app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
     assignorFactory = moduleRef.get(AssignorFactory);
     await app.init();
   });
@@ -40,6 +42,53 @@ describe("AssignorController (e2e)", () => {
         phone: assignor.phone,
         name: assignor.name,
       });
+    });
+  });
+
+  describe("PUT /integrations/assignor/:id", () => {
+    it("should update a assignor", async () => {
+      const assignor = await assignorFactory.makePrismaAssignor();
+
+      const response = await request(app.getHttpServer())
+        .put(`/integrations/assignor/${assignor.id}`)
+        .send({
+          document: "123456789",
+          email: "test@update.com",
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({
+        id: assignor.id.toString(),
+        document: "123456789",
+        email: "test@update.com",
+      });
+
+      const updatedAssignor = await prisma.payable.findUnique({
+        where: { id: assignor.id.toString() },
+      });
+
+      expect(updatedAssignor).toMatchObject({
+        document: "123456789",
+        email: "test@update.com",
+      });
+    });
+  });
+
+  describe("DELETE /integrations/assignor/:id", () => {
+    it("should delete a assignor", async () => {
+      const assignor = await assignorFactory.makePrismaAssignor();
+
+      const response = await request(app.getHttpServer()).delete(
+        `/integrations/assignor/${assignor.id}`,
+      );
+
+      expect(response.status).toBe(200);
+
+      const deletedAssignor = await prisma.assignor.findUnique({
+        where: { id: assignor.id.toString() },
+      });
+
+      expect(deletedAssignor).toBeNull();
     });
   });
 });
